b1646228d9f464ddc8cc7676c512345e68a747fd
Johnny Tang <johnny_tang@hotmail.ch>
2015-12-18 11:24:25 +0100

    18.12.2015
    Johnny Tang

---
+++ index.php
@@ -0,0 +1,27 @@
+  <!DOCTYPE html>
+<html>
+<head>
+    <link href="style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+	<div id="login">
+	</div>
+    	<div id="Nav1">
+        	<ul>
+            		<li><a href="index.php"><strong>TANG</strong> </a></li><br /> 
+            		<li><a href="angebot.html">ANGEBOT</a></li><br />
+            		<li><a href="">REFERENZEN</a></li><br />
+            		<li><a href="">KONTAKT</a></li><br />
+        	</ul>
+        <img src="" id="layout" /> 
+    	</div>
+	<img src="linie.png" id="linie"><br>
+    	<div id="box1"><h1><strong>Tang</strong></h1></div>
+    	<div id="box2"><h1><strong>NEWS</strong></h1></div>
+	<div id="box3"><h1><strong>NEWS</strong></h1></div><br>
+	<div id="footer"><h1><strong>NEWS</strong></h1></div>
+</body>
+<footer>
+    <div id="f1"></div>
+</footer>
+</html>
